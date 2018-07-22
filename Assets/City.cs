28d911a8e12687223db9659856fc3de3e4e35de5
Curtis Holden <smartfolk280@gmail.com>
2018-07-22 02:01:49 -0400

    First Commit

---
+++ Assets/City.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class City : MonoBehaviour
+{
+    public float pop;
+    public float idealpop;
+    public float farmeffper; // farm efficentcy percent
+    public float food;
+    public Color color = new Color(0.2F, 0.3F, 0.4F, 0.5F);
+    public float wealth;
+    int month = 0;
+    //string[] monthn = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ((int)Time.time % 5 == 0 && month != (int)Time.time / 5 % 12 + 1)
+        {
+            month = (int)Time.time / 5 % 12 + 1;
+            //Debug.Log(month);
+            if (food > 0)
+            {
+                pop += 100 * (idealpop / (idealpop + pop));
+            }
+            food -= pop;
+            if (month == 5)
+            {
+                food += (int)((farmeffper + 100) * pop * 12 / 100 * (Random.value / 2 + 0.25) * 2);
+            }
+            if (food < 0)
+            {
+                food = 0;
+            }
+            if (food == 0)
+            {
+                pop = pop / 10 * 9;
+            }
+            if (pop < 1000)
+            {
+                Destroy(this.gameObject);
+            }
+            if (wealth < 0 || food < 0)
+            {
+                GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+            }
+            else
+            {
+                GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+            }
+        }
+    }
+}
---
+++ Assets/Market.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Market : MonoBehaviour
+{
+    public float wealtha; // all money offered
+    public float wealthc;
+    public float popc;
+    public float foodo; // food offered
+    public float foodd; // food demanded
+    public float foodc;
+    public int harvestmon = 5; //month of harvest
+    GameObject[] cities;
+    GameObject city;
+    int month = 0;
+    float foodprice;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (((int)Time.time - 1) % 5 == 0 && month != ((int)Time.time - 1) / 5 % 12 + 1)
+        {
+            int year = (int)(Time.time / 60);
+            foodo = 0;
+            foodd = 0;
+            wealtha = 0;
+            month = (int)Time.time / 5 % 12 + 1;
+            if (month == harvestmon)
+            {
+                Debug.Log(year);
+                Debug.Log("market is open");
+                cities = GameObject.FindGameObjectsWithTag("City");
+                for (int i = 0; i < cities.Length; i++)
+                {
+                    city = cities[i];
+                    float pfood;
+                    foodc = city.GetComponent<City>().food;
+                    wealthc = city.GetComponent<City>().wealth;
+                    popc = city.GetComponent<City>().pop;
+                    pfood = foodc - (int)(popc * 12 * 1.4);
+                    if (pfood < 0 && wealthc > 0)
+                    {
+                        foodd += pfood * -1;
+                        int j = 1;
+                        while (foodc - (int)(popc * (12 - j) * 1.4) < 0)
+                        {
+                            j++;
+                        }
+                        wealtha += wealthc * j / 12;
+                    }
+                    if (pfood > 0)
+                    {
+                        foodo += pfood;
+                    }
+                }
+                // Debug.Log(foodo);
+                // Debug.Log(foodd);
+                // Debug.Log(wealtha);
+                if (wealtha != 0 && foodo != 0)
+                {
+                    // Debug.Log("buy and sell folks");
+                    float foodrate = foodo / foodd;
+                    if (foodrate > 1)
+                    {
+                        foodprice = (wealtha / foodd / foodrate);
+                    }
+                    else
+                    {
+                        foodprice = (wealtha / foodo);
+                    }
+                    Debug.Log(foodprice);
+                    for (int i = 0; i < cities.Length; i++)
+                    {
+                        city = cities[i];
+                        foodc = city.GetComponent<City>().food;
+                        wealthc = city.GetComponent<City>().wealth;
+                        popc = city.GetComponent<City>().pop;
+                        float pfood = foodc - (int)(popc * 12 * 1.4);
+                        if (pfood < 0 && wealthc > 0)
+                        {
+                            if (foodrate > 1)
+                            {
+                                city.GetComponent<City>().food = foodc - (pfood / foodrate);
+                                city.GetComponent<City>().wealth = wealthc + pfood * foodprice;
+                            }
+                            else
+                            {
+                                city.GetComponent<City>().food = foodc - (pfood);
+                                city.GetComponent<City>().wealth = wealthc + pfood * foodrate * foodprice;
+                            }
+                        }
+                        if (pfood > 0)
+                        {
+                            if (foodrate < 1)
+                            {
+                                city.GetComponent<City>().food = foodc - (pfood * foodrate);
+                                city.GetComponent<City>().wealth = wealthc + pfood * foodprice;
+                            }
+                            else
+                            {
+                                city.GetComponent<City>().food = foodc - (pfood);
+                                city.GetComponent<City>().wealth = wealthc + pfood * foodprice / foodrate;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
